c28fdafbabf2c826a9f5b1212f17b6ed1e766a94
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-20 17:40:50 -0500

    made sure create method takes arguments

--- contact.rb
+++ contact.rb
@@ -19,7 +19,7 @@ class Contact
 
   # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
+  def self.create(first_name, last_name, email, note)
     new_contact = self.new(first_name, last_name, email, note)
     @@contacts << new_contact
     return new_contact
